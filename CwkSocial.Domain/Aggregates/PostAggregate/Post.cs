bb56b9a84786a2c6a9f0b0f1cff873f24d1958d3
EnesDemirtas <enesdemirtas255@gmail.com>
2022-03-15 15:10:54 +0300

    feat: use aggregates in domain (rich domain model)

---
+++ CwkSocial.Domain/Aggregates/PostAggregate/Post.cs
@@ -0,0 +1,21 @@
+﻿using CwkSocial.Domain.Aggregates.UserProfileAggregate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CwkSocial.Domain.Aggregates.PostAggregate
+{
+    public class Post
+    {
+        public Guid PostId { get; set; }
+        public Guid UserProfileId { get; set; }
+        public UserProfile UserProfile { get; set; }
+        public string TextContent { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastModified { get; set; }
+        public ICollection<PostComment> Comments { get; set; }
+        public ICollection<PostInteraction> Interaction { get; set; }
+    }
+}
