9c783e3fb693492f4bdcfd7d6d35322759b434d0
EnesDemirtas <enesdemirtas255@gmail.com>
2022-03-15 14:00:32 +0300

    feat: create clean architecture & provide API versioning w/ Swagger & isolate Program.cs file

---
+++ CwkSocial.Api/Controllers/v1/PostsController.cs
@@ -0,0 +1,19 @@
+﻿using CwkSocial.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CwkSocial.Api.Controllers.v1
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class PostsController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var post = new Post { Id = id, Text = "Hello World" };
+            return Ok(post);
+        }
+    }
+}
---
+++ CwkSocial.Api/Controllers/v2/PostsController.cs
@@ -0,0 +1,19 @@
+﻿using CwkSocial.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CwkSocial.Api.Controllers.v2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class PostsController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var post = new Post { Id = id, Text = "Hello Universe" };
+            return Ok(post);
+        }
+    }
+}
